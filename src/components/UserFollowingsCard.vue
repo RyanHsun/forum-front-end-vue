767e903852c8f17db9eecc5c98eced84c03025b9
Ryan Hsun <lenxun.huang@gmail.com>
2021-06-27 09:30:23 +0800

    feat: add component-UserProfileCard

---
+++ src/components/UserFollowingsCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card mb-3">
+    <div class="card-header">
+      <strong>2</strong> followings (追蹤者)
+    </div>
+    <div class="card-body">
+        <a href="/users/1">
+            <img src="https://i.imgur.com/eVfTIsY.jpg" width="60" height="60" class="avatar">
+        </a>
+        <a href="/users/11">
+            <img src="https://i.imgur.com/pIHFJtM.png" width="60" height="60" class="avatar">
+        </a>
+    </div>
+  </div>
+</template>
