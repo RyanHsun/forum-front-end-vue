351bc90c6e64164a584b7a3f2e6d35299d79d8ea
RyanHsun <lenxun.huang@gmail.com>
2021-07-04 06:20:35 +0800

    fix: integrate add/deleteFavorite in RestaurantsTop

--- src/views/RestaurantsTop.vue
+++ src/views/RestaurantsTop.vue
@@ -43,7 +43,7 @@
               v-if="restaurant.isFavorited"
               type="button"
               class="btn btn-danger mr-2"
-              @click.stop.prevent="deleteFavorite(restaurant)"
+              @click.stop.prevent="deleteFavorite(restaurant.id)"
             >
               移除最愛
             </button>
@@ -51,7 +51,7 @@
               v-else
               type="button"
               class="btn btn-primary"
-              @click.stop.prevent="addFavorite(restaurant)"
+              @click.stop.prevent="addFavorite(restaurant.id)"
             >
               加到最愛
             </button>
@@ -66,6 +66,7 @@
 import NavTabs from "./../components/NavTabs"
 import { emptyImageFilter } from './../utils/mixins'
 import restaurantsAPI from './../apis/restaurants'
+import usersAPI from './../apis/users'
 import { Toast } from './../utils/helpers'
 
 export default {
@@ -93,11 +94,55 @@ export default {
         })
       }
     },
-    addFavorite (restaurant) {
-      restaurant.isFavorited = true
+    async addFavorite (restaurantId) {
+      try {
+        const { data } = await usersAPI.addFavorite({ restaurantId })
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+        this.restaurants = this.restaurants
+          .map(restaurant => {
+            if (restaurant.id !== restaurantId) {
+              return restaurant
+            }
+            return {
+              ...restaurant,
+              FavoriteCount: restaurant.FavoriteCount + 1,
+              isFavorited: true
+            }
+          })
+          .sort((a, b) => b.FavoriteCount - a.FavoriteCount)
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法將餐廳加入最愛，請稍後再試'
+        })
+      }
     },
-    deleteFavorite (restaurant) {
-      restaurant.isFavorited = false
+    async deleteFavorite (restaurantId) {
+      try {
+        const { data } = await usersAPI.deleteFavorite({ restaurantId })
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+        this.restaurants = this.restaurants
+          .map(restaurant => {
+            if (restaurant.id !== restaurantId) {
+              return restaurant
+            }
+            return {
+              ...restaurant,
+              FavoriteCount: restaurant.FavoriteCount - 1,
+              isFavorited: false
+            }
+          })
+          .sort((a, b) => b.FavoriteCount - a.FavoriteCount)
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法將餐廳移除最愛，請稍後再試'
+        })
+      }
     }
   }
 };
