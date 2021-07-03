c72932b54891ae1d335b61cc810e7ffa79a64a43
RyanHsun <lenxun.huang@gmail.com>
2021-07-04 05:43:12 +0800

    fix: integrate API of add/deleteFollowing in UsersTop

--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -5,19 +5,19 @@
     <hr />
     <div class="row text-center">
       <div v-for="user in users" :key="user.id" class="col-3">
-        <a href="#">
-          <img :src="user.image || emptyImage" width="140px" height="140px" />
-        </a>
+        <router-link :to="{ name: 'user', params: { id: user.id } }">
+          <img :src="user.image | emptyImage" width="140px" height="140px" />
+        </router-link>
         <h2>{{ user.name }}</h2>
         <span class="badge badge-secondary"
-          >追蹤人數：{{ user.FollowerCount }}</span
+          >追蹤人數：{{ user.followerCount }}</span
         >
         <p class="mt-3">
           <button
             v-if="user.isFollowed"
             type="button"
             class="btn btn-danger"
-            @click.stop.prevent="unFollowed(user)"
+            @click.stop.prevent="deleteFollowing(user.id)"
           >
             取消追蹤
           </button>
@@ -25,7 +25,7 @@
             v-else
             type="button"
             class="btn btn-primary"
-            @click.stop.prevent="followed(user)"
+            @click.stop.prevent="addFollowing(user.id)"
           >
             追蹤
           </button>
@@ -49,10 +49,10 @@ export default {
   data() {
     return {
       users: [],
-    };
+    }
   },
   created() {
-    this.fetchTopUsers();
+    this.fetchTopUsers()
   },
   methods: {
     async fetchTopUsers () {
@@ -74,16 +74,60 @@ export default {
         })
       }
     },
-    followed(user) {
-      console.log("followed");
-      user.FollowerCount++;
-      user.isFollowed = true;
+    async addFollowing (userId) {
+      try {
+        const { data } = await usersAPI.addFollowing({ userId })
+
+        console.log('data', data)
+
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+
+        this.users = this.users.map(user => {
+          if (user.id !== userId) {
+            return user
+          } else {
+            return {
+              ...user,
+              followerCount: user.followerCount + 1,
+              isFollowed: true
+            }
+          }
+        })
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法加入追蹤，請稍後再試'
+        })
+      }
     },
-    unFollowed(user) {
-      console.log("un followed");
-      user.FollowerCount--;
-      user.isFollowed = false;
-    },
-  },
-};
+    async deleteFollowing (userId) {
+      try {
+        const { data } = await usersAPI.deleteFollowing({ userId })
+
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+
+        this.users = this.users.map(user => {
+          if (user.id !== userId) {
+            return user
+          } else {
+            return {
+              ...user,
+              followerCount: user.followerCount - 1,
+              isFollowed: false
+            }
+          }
+        })
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取消追蹤，請稍後再試'
+        })
+      }
+    }
+  }
+}
 </script>
