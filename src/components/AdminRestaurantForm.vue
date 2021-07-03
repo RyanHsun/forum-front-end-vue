9a087a8ac03cf864415f0e922cb562f9269f0845
RyanHsun <lenxun.huang@gmail.com>
2021-07-04 06:45:24 +0800

    fix: integrate admin.catgories.get() in AdminRestaurantForm

--- src/components/AdminRestaurantForm.vue
+++ src/components/AdminRestaurantForm.vue
@@ -1,5 +1,8 @@
 <template>
-  <form @submit.stop.prevent="handleSubmit">
+  <form 
+    v-show="!isLoading"
+    @submit.stop.prevent="handleSubmit"
+  >
     <div class="form-group">
       <label for="name">Name</label>
       <input
@@ -114,34 +117,8 @@
 </template>
 
 <script>
-const dummyData = {
-  categories: [
-    {
-      id: 1,
-      name: '中式料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    },
-    {
-      id: 2,
-      name: '日本料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    },
-    {
-      id: 3,
-      name: '義大利料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    },
-    {
-      id: 4,
-      name: '墨西哥料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    }
-  ]
-}
+import adminAPI from './../apis/admin'
+import { Toast } from './../utils/helpers'
 
 export default {
   props: {
@@ -165,7 +142,8 @@ export default {
       restaurant: {
         ...this.initialRestaurant
       },
-      categories: []
+      categories: [],
+      isLoading: true
     }
   },
   created () {
@@ -176,8 +154,22 @@ export default {
     }
   },
   methods: {
-    fetchCategories () {
-      this.categories = dummyData.categories
+    async fetchCategories () {
+      try {
+        const { data } = await adminAPI.categories.get()
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+        this.categories = data.categories
+        this.isLoading = false
+      } catch (error) {
+        console.error(error.message)
+        this.isLoading = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳類別，請稍後再試'
+        })
+      }
     },
     handleFileChange (e) {
       const { files } = e.target
