55d96f182e5c688ac1200827e1d2ab3b1088d4cc
RyanHsun <lenxun.huang@gmail.com>
2021-07-05 01:12:14 +0800

    fix: use isProcessing to prevent user double submit in RestaurantCard

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -6,8 +6,8 @@
         :src="restaurant.image | emptyImage"
         alt="Card image cap"
         width="286px"
-        height="auto"
-      />
+        height="180px"
+      >
       <div class="card-body">
         <p class="card-text title-wrap">
           <router-link
@@ -17,7 +17,7 @@
           </router-link>
         </p>
         <span class="badge badge-secondary">{{
-          restaurant.Category.name
+          restaurant.Category ? restaurant.Category.name : '未分類'
         }}</span>
         <p class="card-text text-truncate">
           {{ restaurant.description }}
@@ -26,6 +26,7 @@
       <div class="card-footer">
         <button
           v-if="restaurant.isFavorited"
+          :disabled="isProcessing"
           type="button"
           class="btn btn-danger btn-border favorite mr-2"
           @click.stop.prevent="deleteFavorite(restaurant.id)"
@@ -35,6 +36,7 @@
         <button
           v-else
           type="button"
+          :disabled="isProcessing"
           class="btn btn-primary btn-border favorite mr-2"
           @click.stop.prevent="addFavorite(restaurant.id)"
         >
@@ -43,6 +45,7 @@
         <button
           v-if="restaurant.isLiked"
           type="button"
+          :disabled="isProcessing"
           class="btn btn-danger like mr-2"
           @click.stop.prevent="deleteLike(restaurant.id)"
         >
@@ -51,6 +54,7 @@
         <button
           v-else
           type="button"
+          :disabled="isProcessing"
           class="btn btn-primary like mr-2"
           @click.stop.prevent="addLike(restaurant.id)"
         >
@@ -62,41 +66,39 @@
 </template>
 
 <script>
-import { emptyImageFilter } from "./../utils/mixins";
-import usersAPI from "./../apis/users";
-import { Toast } from "./../utils/helpers";
-
+import { emptyImageFilter } from './../utils/mixins'
+import usersAPI from './../apis/users'
+import { Toast } from './../utils/helpers'
 export default {
+  name: 'RestaurantCard',
   mixins: [emptyImageFilter],
   props: {
     initialRestaurant: {
       type: Object,
-      required: true,
-    },
+      required: true
+    }
   },
-  data() {
+  data () {
     return {
       restaurant: this.initialRestaurant,
-    };
+      isProcessing: false
+    }
   },
   methods: {
     async addFavorite (restaurantId) {
       try {
         this.isProcessing = true
-        const { data, statusText } = await usersAPI.addFavorite({
-          restaurantId
-        })
-
-        if (statusText !== 'OK' || data.status !== 'success') {
-          throw new Error(statusText)
+        const { data } = await usersAPI.addFavorite({ restaurantId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
         }
-
         this.restaurant = {
           ...this.restaurant,
           isFavorited: true
         }
         this.isProcessing = false
       } catch (error) {
+        console.error(error.message)
         this.isProcessing = false
         Toast.fire({
           icon: 'error',
@@ -107,70 +109,63 @@ export default {
     async deleteFavorite (restaurantId) {
       try {
         this.isProcessing = true
-        const { data, statusText } = await usersAPI.deleteFavorite({
-          restaurantId
-        })
-
-        if (statusText !== 'OK' || data.status !== 'success') {
-          throw new Error(statusText)
+        const { data } = await usersAPI.deleteFavorite({ restaurantId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
         }
-
         this.restaurant = {
           ...this.restaurant,
           isFavorited: false
         }
         this.isProcessing = false
       } catch (error) {
+        console.error(error.message)
         this.isProcessing = false
         Toast.fire({
           icon: 'error',
-          title: '無法將餐廳從最愛移除，請稍後再試'
+          title: '無法將餐廳移除最愛，請稍後再試'
         })
       }
     },
     async addLike (restaurantId) {
       try {
         this.isProcessing = true
-        const { data, statusText } = await usersAPI.addLike({ restaurantId })
-
-        if (statusText !== 'OK' || data.status !== 'success') {
-          throw new Error(statusText)
+        const { data } = await usersAPI.addLike({ restaurantId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
         }
-
         this.restaurant = {
           ...this.restaurant,
           isLiked: true
         }
         this.isProcessing = false
       } catch (error) {
+        console.error(error.message)
         this.isProcessing = false
         Toast.fire({
           icon: 'error',
-          title: '無法按讚，請稍後再試'
+          title: '無法對餐廳按讚，請稍後再試'
         })
       }
     },
     async deleteLike (restaurantId) {
       try {
         this.isProcessing = true
-        const { data, statusText } = await usersAPI.deleteLike({
-          restaurantId
-        })
-
-        if (statusText !== 'OK' || data.status !== 'success') {
-          throw new Error(statusText)
+        const { data } = await usersAPI.deleteLike({ restaurantId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
         }
-
         this.restaurant = {
           ...this.restaurant,
           isLiked: false
         }
         this.isProcessing = false
       } catch (error) {
+        console.error(error.message)
         this.isProcessing = false
         Toast.fire({
           icon: 'error',
-          title: '無法取消按讚，請稍後再試'
+          title: '無法對取消讚，請稍後再試'
         })
       }
     }
@@ -185,23 +180,19 @@ export default {
   color: #bd2333;
   background-color: transparent;
 }
-
 .btn,
 .btn-border.btn:hover {
   margin: 7px 14px 7px 0;
 }
-
 .card {
   margin-bottom: 2rem !important;
 }
 .card-img-top {
-  background-color: #EFEFEF;
+  background-color: #efefef;
 }
-
 .card-body {
   padding: 17.5px;
 }
-
 .card-footer {
   padding: 9px 17.5px;
   border-color: rgb(232, 232, 232);
