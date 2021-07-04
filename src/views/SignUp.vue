37e499202eb55002913f7e62cdc5ceb18be03388
RyanHsun <lenxun.huang@gmail.com>
2021-07-04 09:39:31 +0800

    Add API - authorization.signup and integrate in SignUp

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -87,6 +87,9 @@
 </template>
 
 <script>
+import authorizationAPI from './../apis/authorization'
+import { Toast } from './../utils/helpers'
+
 export default {
   data () {
     return {
@@ -97,14 +100,52 @@ export default {
     }
   },
   methods: {
-    handleSubmit () {
-      const data = JSON.stringify({
-        name: this.name,
-        email: this.email,
-        password: this.password,
-        passwordCheck: this.passwordCheck
-      })
-      console.log('data:', data)
+    async handleSubmit () {
+      try {
+        if (
+          !this.name ||
+          !this.email ||
+          !this.password ||
+          !this.passwordCheck
+        ) {
+          Toast.fire({
+            icon: 'warning',
+            title: '請確認已填寫所有欄位'
+          })
+          return
+        }
+        if (this.password !== this.passwordCheck) {
+          Toast.fire({
+            icon: 'warning',
+            title: '兩次輸入的密碼不同'
+          })
+          this.passwordCheck = ''
+          return
+        }
+        const { data } = await authorizationAPI.signUp({
+          name: this.name,
+          email: this.email,
+          password: this.password,
+          passwordCheck: this.passwordCheck
+        })
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+
+        Toast.fire({
+          icon: 'success',
+          title: data.message
+        })
+        // 成功登入後轉址到登入頁
+        this.$router.push('/signin')
+
+      } catch (error) {
+        Toast.fire({
+          icon: 'warning',
+          title: '無法註冊帳戶，請稍後再試'
+        })
+      }
+
     }
   }
 }
